3c2d8f203f73685175437354a98b47254bbb6195
Raúl Scio <raulmoreno899@gmail.com>
2019-09-04 14:40:08 -0500

    Tarea actualizada con subcarpetas

--- FileSystem.js
+++ FileSystem.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const print = require('./printer')
+const path = require('path')
 
-const fromUrl = './Original Items'
-const pasteUrl = './Pasted Items'
+const fromUrl = './Original'
+const pasteUrl = './Pasted'
 const{ COPYFILE_EXCL } = fs.constants
 
 
@@ -10,23 +11,42 @@ if (!fs.existsSync(pasteUrl)) {
     fs.mkdirSync(pasteUrl, 0744)
 }
 
-let dirBuf = Buffer.from(fromUrl)
-let files = fs.readdirSync(fromUrl)
 
 print.ln("Iniciando a copiar los archivos");
 	
 var errores = false
-for (var i = 0; i < files.length ; i++) {
-	const fileName = files[i]
-	try {
-		fs.copyFileSync(fromUrl+"\\"+fileName, pasteUrl+"\\"+fileName, COPYFILE_EXCL)
-		print.show(fileName)
-	} catch (err){
-		errores = true
-		print.showError("Ocurrio error al copiar el archivo \"" + fileName+ "\"")
-	}
-}
+copyFiles(fromUrl, pasteUrl)
+
 if (!errores)
 	print.ln("Archivos copiados correctamente")
 else
 	print.showError("Se sucitaron errores al copiar archivos")
+
+
+
+
+
+
+function isFile(pathItem) {
+  return !!path.extname(pathItem);
+}
+
+function copyFiles(source, destination){
+	let files = fs.readdirSync(source)
+	for (var i = 0; i < files.length ; i++) {
+		const fileName = files[i]
+		if(isFile(source+''+fileName)){
+			try {
+				fs.copyFileSync(source+"\\"+fileName, destination+"\\"+fileName, COPYFILE_EXCL)
+				print.show(fileName)
+			} catch (err){
+				errores = true
+				print.showError("Ocurrio error al copiar el archivo \"" + fileName+ "\"")
+			}
+		}else{
+			if (!fs.existsSync(destination+'/'+fileName))
+			    fs.mkdirSync(destination+'/'+fileName);
+			copyFiles(source+'/'+fileName, destination+'/'+fileName)
+		}
+	}
+}
