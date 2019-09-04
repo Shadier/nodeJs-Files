4c6f71dff77cb88b399c31336daf24724017d42e
Raúl Scio <raulmoreno899@gmail.com>
2019-09-04 10:58:46 -0500

    mostrando mensajes antes y despues

--- FileSystem.js
+++ FileSystem.js
@@ -3,6 +3,8 @@ const print = require('./printer')
 
 const fromUrl = './Original Items'
 const pasteUrl = './Pasted Items'
+const{ COPYFILE_EXCL } = fs.constants
+
 
 if (!fs.existsSync(pasteUrl)) {
     fs.mkdirSync(pasteUrl, 0744)
@@ -11,12 +13,20 @@ if (!fs.existsSync(pasteUrl)) {
 let dirBuf = Buffer.from(fromUrl)
 let files = fs.readdirSync(fromUrl)
 
+print.ln("Iniciando a copiar los archivos");
+	
+var errores = false
 for (var i = 0; i < files.length ; i++) {
 	const fileName = files[i]
-	fs.copyFile(fromUrl+"\\"+fileName, pasteUrl+"\\"+fileName, (err) => {
-		if (err) 
-			print.showError(err)
-		else
-			print.show(fileName)
-	})
+	try {
+		fs.copyFileSync(fromUrl+"\\"+fileName, pasteUrl+"\\"+fileName, COPYFILE_EXCL)
+		print.show(fileName)
+	} catch (err){
+		errores = true
+		print.showError("Ocurrio error al copiar el archivo \"" + fileName+ "\"")
+	}
 }
+if (!errores)
+	print.ln("Archivos copiados correctamente")
+else
+	print.showError("Se sucitaron errores al copiar archivos")
--- printer.js
+++ printer.js
@@ -1,5 +1,8 @@
 const colors = require('colors')
 
+const ln = (msg) => {
+	console.log(colors.white(msg))
+}
 
 const show = (fileName) => {
     console.log(colors.white('"')+colors.blue(fileName.bold.italic.underline)+colors.white('" was copied ')+colors.green("successfully"))
@@ -11,5 +14,6 @@ const showError = (msg) => {
 
 module.exports = {
 	showError, 
-	show
+	show,
+	ln
 }
